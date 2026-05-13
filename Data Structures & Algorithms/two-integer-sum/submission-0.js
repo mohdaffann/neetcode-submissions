435d71b68be7bcbe7db5123d25b2e34c4f73a69d
mohdaffann <muhammadaffanpvt@gmail.com>
2026-05-13 22:41:16 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+       let map = new Map();
+       for(let i = 0 ; i<nums.length ; i++){
+         const req = target - nums[i];
+         if(map.has(req)) return [map.get(req) , i];
+         map.set(nums[i] , i)
+       }
+    }
+}
