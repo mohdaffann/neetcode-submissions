cdc23f687ad73a22fdebe697a060a59a8ffd34fb
mohdaffann <muhammadaffanpvt@gmail.com>
2026-05-13 19:18:01 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let str = s.replace(/[^a-zA-Z0-9]/g , '').toLowerCase();
+        let left = 0 
+        let right = str.length-1;
+        while(left<right){
+            if(str[left]!==str[right]) return false;
+            left ++;
+            right--;
+        }
+        return true
+    }
+}
