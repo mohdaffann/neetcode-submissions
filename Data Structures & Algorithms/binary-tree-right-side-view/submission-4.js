967285684ebc419ffe6a34d58921eec8e36b418b
mohdaffann <muhammadaffanpvt@gmail.com>
2026-05-24 16:10:36 +0530

    Add: binary-tree-right-side-view - submission-4

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-4.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        if(root===null) return []
+    let res = [];
+    let q = [root];
+    while(q.length){
+        let level = [];
+        let len = q.length;
+        for(let i=0; i<len ; i++){
+            let node = q.shift();
+            level.push(node.val);
+            if(node.left) q.push(node.left)
+            if(node.right) q.push(node.right)
+        }
+        res.push(level.pop())
+    }
+    return res;
+    }
+}
